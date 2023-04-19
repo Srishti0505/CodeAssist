71bb2d6e89b1f505f2e8b1889ddf32be255cebf1
Srishti0505 <ssrishti726@gmail.com>
2023-04-19 19:54:00 +0530

    first commit

---
+++ backend/connection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const url ='mongodb+srv://ssrishti726:6388422352@cluster0.xjrocys.mongodb.net/mydatabase?retryWrites=true&w=majority';
+
+ mongoose.connect(url) 
+ .then((result) => {
+   //console.log(result);
+    console.log('database connected');
+    
+ })
+ .catch((err) => {
+    console.error(err);
+    
+ }); 
+ 
+
+ module.exports = mongoose;
---
+++ backend/routers/userRouter.js
@@ -0,0 +1,44 @@
+const Model = require('../Models/userModel');
+const { Router } =require('express');
+const router =Router();
+
+//add user data
+router.post('/add', (req,res) => {
+    console.log(req.body);
+    res.send('Response from Add');
+
+});
+
+router.post('/authenticate',(req, res) => {
+    Model.findOne(req.body)
+    .then((result) => {
+
+        if(result) res.json(result);
+        else res.status(401).json({message: 'Invalid Credentials' });
+
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+        });
+    });
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
