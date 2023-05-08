b998ce844f16c6430eca22f790124daf716d3654
Srishti0505 <ssrishti726@gmail.com>
2023-05-08 19:24:12 +0530

    ..

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+const api_config = {
+    OPENAI_API_KEY : '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'
+};
+
+module.exports = api_config;
--- frontend/src/components/main/Home.js
+++ frontend/src/components/main/Home.js
@@ -1,12 +1,82 @@
-import React from 'react'
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+	<style>
+		/* CSS style rules go here */
+		body {
+			font-family: Arial, sans-serif;
+			margin: 0;
+			padding: 0;
+		}
 
-const Home = () => {
-  return (
-    <div>
-    <h1 classname="" style={{ color:'red',backgroundcolor: 'yellow'}}>My react App </h1>
-    <input type="text" />
-      
-      </div>
-  )
-}
-export default Home
+		header {
+			background-color: #333;
+			color: #fff;
+			padding: 10px;
+			text-align: center;
+		}
+
+		nav {
+			background-color: #f2f2f2;
+			border: 1px solid #ccc;
+			padding: 10px;
+		}
+
+		nav ul {
+			list-style-type: none;
+			margin: 0;
+			padding: 0;
+		}
+
+		nav li {
+			display: inline-block;
+			margin-right: 10px;
+		}
+
+		nav a {
+			color: #333;
+			text-decoration: none;
+			padding: 5px;
+		}
+
+		nav a:hover {
+			background-color: #333;
+			color: #fff;
+		}
+
+		section {
+			padding: 10px;
+			margin: 10px;
+			border: 1px solid #ccc;
+			background-color: #fff;
+		}
+
+		footer {
+			background-color: #333;
+			color: #fff;
+			padding: 10px;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<h1>My Website</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</nav>
+	<section>
+		<h2>Welcome to My Website</h2>
+		<p>This is a sample home page for my website. It contains a header, navigation bar, and a main content section.</p>
+	</section>
+	<footer>
+		<p>&copy; 2023 My Website. All rights reserved.</p>
+	</footer>
+</body>
+</html>
