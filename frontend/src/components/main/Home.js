81e5d5a2584f4d2d3e5cce3cf9095d7342ec00b1
m-triple-m <triplem656@gmail.com>
2023-05-09 19:22:10 +0530

    ..

--- frontend/src/components/main/Home.js
+++ frontend/src/components/main/Home.js
@@ -1,62 +1,82 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
     <>
-    sectio.nav
-    h1 FRONTEND TRENDS
-    h3.span.loader
-      span.m B
-      span.m E
-      span.m N
-      span.m E
-      span.m F
-      span.m I
-      span.m T
-      span.m S
-      span.m &nbsp;
-      span.m o
-      span.m f
-      span.m &nbsp;
-      span.m T
-      span.m E
-      span.m C
-      span.m H
-      span.m N
-      span.m O
-      span.m L
-      span.m O
-      span.m G
-      span.m I
-      span.m E
-      span.m S
-    .nav-container
-      a.nav-tab(href="#tab-svelte") SVELTE
-      a.nav-tab(href="#tab-esbuild") ESBUILD
-      a.nav-tab(href="#tab-next") NEXT.JS
-      a.nav-tab(href="#tab-typescript") TYPESCRIPT
-      a.nav-tab(href="#tab-vite") VITE
-      span.nav-tab-slider
-  main.main
-    section#tab-svelte.slider
-      h1 SVELTE
-      h2 another frontend JS framework
-    section#tab-esbuild.slider
-      h1 ESBUILD
-      h2 an extremely fast JavaScript bundler
-    section#tab-next.slider
-      h1 NEXT.JS
-      h2 framework for Production
-    section#tab-typescript.slider
-      h1 TYPESCRIPT
-      h2 giving you better tooling at any scale
-    section#tab-vite.slider
-      h1 VITE
-      h2 a frontend build tool
-  canvas.background
-  
- 
-  </>
+  <sectio className="nav">
+    <h1>FRONTEND TRENDS</h1>
+    <h3 className="span loader">
+      <span className="m">B</span>
+      <span className="m">E</span>
+      <span className="m">N</span>
+      <span className="m">E</span>
+      <span className="m">F</span>
+      <span className="m">I</span>
+      <span className="m">T</span>
+      <span className="m">S</span>
+      <span className="m">&nbsp;</span>
+      <span className="m">o</span>
+      <span className="m">f</span>
+      <span className="m">&nbsp;</span>
+      <span className="m">T</span>
+      <span className="m">E</span>
+      <span className="m">C</span>
+      <span className="m">H</span>
+      <span className="m">N</span>
+      <span className="m">O</span>
+      <span className="m">L</span>
+      <span className="m">O</span>
+      <span className="m">G</span>
+      <span className="m">I</span>
+      <span className="m">E</span>
+      <span className="m">S</span>
+    </h3>
+    <div className="nav-container">
+      <a className="nav-tab" href="#tab-svelte">
+        SVELTE
+      </a>
+      <a className="nav-tab" href="#tab-esbuild">
+        ESBUILD
+      </a>
+      <a className="nav-tab" href="#tab-next">
+        NEXT.JS
+      </a>
+      <a className="nav-tab" href="#tab-typescript">
+        TYPESCRIPT
+      </a>
+      <a className="nav-tab" href="#tab-vite">
+        VITE
+      </a>
+      <span className="nav-tab-slider" />
+    </div>
+  </sectio>
+  <main className="main">
+    <section className="slider" id="tab-svelte">
+      <h1>SVELTE</h1>
+      <h2>another frontend JS framework</h2>
+    </section>
+    <section className="slider" id="tab-esbuild">
+      <h1>ESBUILD</h1>
+      <h2>an extremely fast JavaScript bundler</h2>
+    </section>
+    <section className="slider" id="tab-next">
+      <h1>NEXT.JS</h1>
+      <h2>framework for Production</h2>
+    </section>
+    <section className="slider" id="tab-typescript">
+      <h1>TYPESCRIPT</h1>
+      <h2>giving you better tooling at any scale</h2>
+    </section>
+    <section className="slider" id="tab-vite">
+      <h1>VITE</h1>
+      <h2>a frontend build tool</h2>
+    </section>
+  </main>
+  <canvas className="background" />
+  {/* SGVsbG8hIE15IG5hbWUgaXMgU2FyYSBNYXphbC4gV2VsY29tZSB0byBteSBDb2RlUGVuIQ== */}
+</>
+
   )
 }
 
