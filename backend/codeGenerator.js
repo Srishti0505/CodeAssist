d98bde96959352f87a1a00d9244e4784715f24da
m-triple-m <triplem656@gmail.com>
2023-05-23 19:05:35 +0530

    ..

--- backend/codeGenerator.js
+++ backend/codeGenerator.js
@@ -9,6 +9,6 @@ openai.createChatCompletion({
   model: "gpt-3.5-turbo",
   messages: [{role: "user", content: "Write HTML, JS and bootstrap 5 code for a simple contacts page that takes in a name, email, and message and sends it to a database."}],
 }).then((response) => {
-    // console.log(response.data);
+    // consokle.log(response.data);
     console.log(response.data.choices[0].message);
 })
