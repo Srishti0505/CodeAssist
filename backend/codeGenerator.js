36aad808f0096e78952f428ccee7691710a98a85
Srishti0505 <ssrishti726@gmail.com>
2023-05-26 16:22:45 +0530

    hhhuhgu

--- backend/codeGenerator.js
+++ backend/codeGenerator.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: "jsbdhbh",
+  apiKey: "jhbbjhjhh",
 });
 const openai = new OpenAIApi(configuration);
 
